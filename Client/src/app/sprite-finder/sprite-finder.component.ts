e4132a5ac3fe0db56b9845f3328e1b2b365628f8
Dmitriy Ilchenko <d.ilchenko@plarium.com>
2019-05-06 20:33:46 +0300

    file drop basis

--- Client/src/app/sprite-finder/sprite-finder.component.ts
+++ Client/src/app/sprite-finder/sprite-finder.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-sprite-finder',
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SpriteFinderComponent implements OnInit {
 
   filters: any;
+  showFileDropArea: boolean;
 
   constructor() { }
 
@@ -16,5 +17,37 @@ export class SpriteFinderComponent implements OnInit {
 
   onFilterChange(newFilters: any) {
     this.filters = newFilters;
+  }
+
+  dragCounter = 0;
+  @HostListener('window:dragover', ['$event'])
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'copy';
+  }
+
+  @HostListener('window:dragenter', ['$event'])
+  onDragEnter(event: DragEvent) {
+    console.log('enter');
+    this.showFileDropArea = true;
+    this.dragCounter++;
+  }
+
+  @HostListener('window:dragleave', ['$event'])
+  onDragLeave(event: DragEvent) {
+    console.log('leave');
+    this.dragCounter--;
+    if(this.dragCounter === 0)
+      this.showFileDropArea = false;
+  }
+
+  @HostListener('window:drop', ['$event'])
+  onDragDrop(event: DragEvent) {
+    console.log('drop');
+    event.preventDefault();
+    event.stopPropagation();
+    this.showFileDropArea = false;
+    this.dragCounter = 0;
+    return true;
   }
 }
