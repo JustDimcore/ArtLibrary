a51e631ca6e412dd3d0ea19d4c7048b4e31cd345
dimcore <dimcore.dev@gmail.com>
2019-05-24 03:21:16 +0300

    Added adblock checking service. Removed unused code in AppComponent.

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,28 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {filter} from 'rxjs/internal/operators';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'app';
-
-  code: string;
-
-  constructor(private route: ActivatedRoute) {
-  }
-
-  ngOnInit() {
-    this.route.queryParams.pipe(
-      filter(params => params.code)
-    )
-    .subscribe((params: any) => {
-      console.log(params);
-      this.code = params.code;
-      console.log(this.code);
-    });
-  }
+export class AppComponent {
 }
